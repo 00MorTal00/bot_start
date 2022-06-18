f5e405c7ad04757493ecee7cd6b8c20e55eeda4b
00MorTal00 <dima120402@gmail.com>
2022-06-18 23:10:01 +0300

    add to .gitignore .txt and add game who namber biger

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
+from email import message
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from random import randint
 
 import settings
 
@@ -14,11 +16,34 @@ def talk_to_me(update, context):
     print(text)
     update.message.reply_text(text)
 
+def play_random_number(user_number):
+    bot_number = randint(user_number - 10, user_number + 10)
+    if user_number > bot_number:
+        message = f"Ваше число {user_number}, мое {bot_number}, вы выиграли"
+    elif user_number == bot_number:
+        message = f"Ваше число {user_number}, мое {bot_number}, ничья"
+    else:
+        message = f"Ваше число {user_number}, мое {bot_number}, вы проиграли"
+    return message
+
+def guess_number(update, context):
+    print(context.args)
+    if context.args:
+        try:
+            user_namber = int(context.args[0])
+            message = play_random_number(user_namber)
+        except (TypeError, ValueError):
+            message = "Введите целое число"
+    else:
+        message = "Введите число"
+    update.message.reply_text(message)
+
 def main():
     mybot = Updater(settings.APY_KEY, use_context=True)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("guess", guess_number))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал')
